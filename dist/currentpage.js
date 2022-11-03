afdce50cfada17aff35b518cb3f5e21d9a523f2c
girobusan <girobusan@gmail>
2022-11-03 16:26:49 +0300

    +monospace font

--- dist/currentpage.js
+++ dist/currentpage.js
@@ -1,1 +1,1 @@
-console.info("Current Link user script"),window.MDS.addUpdater((function(e,r){e||(e=window.MDS.current.path),document.querySelectorAll("a").forEach((r=>{r.getAttribute("href")==e?r.classList.add("currentPage"):r.classList.remove("currentPage")}))}));
+console.info("Current Link user script"),window.MDS.addUpdater((function(e,r){console.log("check if current link",e),e||(e=window.MDS.current.path),document.querySelectorAll("a").forEach((r=>{r.getAttribute("href")==e?r.classList.add("currentPage"):r.classList.remove("currentPage")}))}));
--- src/viewer.js
+++ src/viewer.js
@@ -7,6 +7,10 @@ import {saveToDisk} from "./fileops";
 
 window.MDS = {
   usr: {},
+  editMode: false,
+  current: null,
+
+  //default settings
   settings: {
     "title": "MD Site",
     "mdDir": "md/",
@@ -18,22 +22,22 @@ window.MDS = {
     "userScriptsPath" : "",
     "usr" : {}
   },
+
   page404:  function(l){
-  return{
-    html: `<h1>${l}</h1><p>This page is not created yet. Click "edit" to create it.</p>`,
-    markdown: ""
-  }
+    return{
+      html: `<h1>${l}</h1><p>This page is not created yet. Click "edit" to create it.</p>`,
+      markdown: ""
+    }
   },
-  updaters: {},
-  updID: 1,
-  editMode: false,
-  current: null,
 
   makePath: function(fn){
     const dp = window.MDS.settings.mdDir ;
-    // return fn;
     return dp ? dp + "" + fn : fn;
   },
+
+  //View updaters 
+  updaters: {},
+  updID: 1,
   addUpdater : function(f){
     const id =  window.MDS.updID
     window.MDS.updaters[id] =  f;
@@ -43,29 +47,41 @@ window.MDS = {
   removeUpdater : function(id){
    delete(window.MDS.updaters[id]);
   },
+
+  //run given function once
   once(f){
-     // console.log("run once");
      f(window.MDS);
   },
+
+  //run given function once WHEN window become active
   whenActive(f){
     const handler = function(){ f(); window.removeEventListener("focus" , this) }
        window.addEventListener("focus", handler) ;
     },
+
   updateViews : function(path,content){
     Object.values(window.MDS.updaters).forEach(f=>f(path,content));
   },
+
+  //re-run view updaters
   refresh(){
      window.MDS.updateViews(window.MDS.current.path,window.MDS.current.content);
   },
+
+  //force reload current file from server
   reload(){
      window.MDS.showPath(window.MDS.current.path , {cache: "reload"});
   },
+
+  //show given content
   showContent: function(p,c){ //show page
      // console.info("Showing" , p)
      window.MDS.current = { path: p , content: c , saved: true }
      if(!window.MDS.editMode){ window.scrollTo(0,0) } 
      window.MDS.updateViews(p,c);
   },
+
+  //show content of the file by given path
   showPath: function(p , fetchOpts){
   // console.log('showPath' , p);
     
@@ -76,12 +92,13 @@ window.MDS = {
     })
     .catch(e=>{
       console.info("No such file" , filePath , e);
-      //show 404 page?
        window.MDS.showContent( p , MDS.page404(p) );
     })
   },
+
+  //go to location 
   go: function(p){ //go to the location
-     //check if external
+     //check and handle external links
      if(( p.match(/^( http|ftp )(s)?\:/i) ) ||  //starts with http
      ( !p.match(/\.(md|markdown)$/i) )  || //does not end with md extension
        p.match(/\#/) //contains hash
@@ -94,16 +111,19 @@ window.MDS = {
      if(p){
        window.MDS.showPath( p );
        history.pushState({ path: p , isMMDSstate:true } , null , "#!"+p) ; //no URL here
+    //handle empty path (=>indexFile)
      }else{
-      console.log("replace state")
        window.MDS.showPath( window.MDS.settings.indexFile );
-       history.replaceState({ path: window.MDS.settings.indexFile , isMMDSstate:true } , null , "#!"+window.MDS.settings.indexFile) ; 
+       history.pushState({ path: window.MDS.settings.indexFile , isMMDSstate:true } , null , "#!"+window.MDS.settings.indexFile) ; 
 
      }
   },
 
+  //basic app actions
   action: {
-    
+
+    // window.MDS.go, //must be moved here
+
     edit: ()=>{
       window.MDS.editMode = true;
       const s = document.createElement("script");
@@ -111,16 +131,16 @@ window.MDS = {
       document.body.appendChild(s)
     },
     save: ()=>{
-    console.info("Saving without editing")  ;
-    saveToDisk(window.MDS.current.path, window.MDS.current.content.markdown)
+      console.info("Saving without editing")  ;
+      saveToDisk(window.MDS.current.path, window.MDS.current.content.markdown)
 
     },
     setMenu: (p)=>{
 
       const sidebarNode = document.getElementById("menu");
+      if(!p){ return };
       getContent(window.MDS.makePath(window.MDS.settings.menuFile))
       .then(r=>{sidebarNode.innerHTML=r.html; window.MDS.refresh()})
-      
     .catch( e=>console.log( "no sidebar" ,e ))
 
     }
@@ -133,24 +153,21 @@ window.MDS = {
 async function startSite(){
   console.info("My Markdown Site, version" , VERSION);
   const MDS = window.MDS;
-  //
+  
   //read settings
   const sjr = await fetch("settings.json")
   .catch(e=>{console.log("Can not access settings.json")});
 
   const settingsJSON =sjr&&( sjr.status=='200' ) ? await sjr.json() :  ""
-
-  // console.log("what we've got" , settingsJSON );
   const userSettings = settingsJSON || window.settings ;
-
   const settings =  userSettings ? Object.assign(MDS.settings , userSettings) : MDS.settings ;
   MDS.settings = settings;
   // console.log("settings" , settings)
-  //
-  //find DOM nodes
+  
+  //find  required DOM nodes
   const sidebarNode = document.getElementById("menu");
   const contentNode = document.getElementById("content");
-  //
+  
   //add menu
   if(sidebarNode)
   {
@@ -166,6 +183,7 @@ async function startSite(){
     console.info(i , "Loading user script:" , s);
     document.head.appendChild(sc);
   } )
+
   //let window title follow document header
   MDS.addUpdater((p,c)=>{
     const ttr = /<h1>(.*?)<\/h1>/i ;
@@ -177,6 +195,7 @@ async function startSite(){
     }
 
   })
+
   //add and show content
   if(contentNode){
     // console.log("Content node found")
@@ -185,14 +204,14 @@ async function startSite(){
     //
     //find and display start location
     if(window.location.hash){
-       history.replaceState( { path: window.location.hash.substring(2) , isMMDSstate: true} , null  );
-       MDS.showPath(window.location.hash.substring(2))
+      history.replaceState( { path: window.location.hash.substring(2) , isMMDSstate: true} , null  );
+      MDS.showPath(window.location.hash.substring(2))
     }else{
-       history.replaceState({ path: window.MDS.settings.indexFile , isMMDSstate:true } , null , "#!"+window.MDS.settings.indexFile) ; 
-       MDS.showPath(MDS.settings.indexFile)
+      history.replaceState({ path: window.MDS.settings.indexFile , isMMDSstate:true } , null , "#!"+window.MDS.settings.indexFile) ; 
+      MDS.showPath(MDS.settings.indexFile)
     }
   }else{
-     console.error("Content node not found")
+    console.error("Content node not found")
   }
 
   //add listeners
@@ -210,7 +229,7 @@ async function startSite(){
 
   }
   function restoreState(s){
-    if(!s.state.isMMDSstate){ return }
+    if(!s.state.isMMDSstate){ return }  // we restore only MMDS states
     window.MDS.showPath(s.state.path ) ;
   }  
   function syncHash(){
@@ -222,15 +241,12 @@ async function startSite(){
     window.addEventListener("hashchange" ,   syncHash);
 
     window.MDS.cleanUp = ()=>{
-
-      // window.removeEventListener("click", detectClicks);
       window.removeEventListener("popstate" , restoreState );
-      // window.removeEventListener("hashchange" ,   syncHash);
       window.removeEventListener("DOMContentLoaded", startSite);
-      // delete(window.MDS);
     }
 
   }
-    window.addEventListener("DOMContentLoaded", startSite);
+
+  window.addEventListener("DOMContentLoaded", startSite);
 
 
